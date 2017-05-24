6320b19a19cd5f5d727a07d1671aa3346eee9eb7
td2014 <anthonyd2004@gmail.com>
2017-05-24 04:14:18 -0700

    added scrambling for train data

--- nn_proc.py
+++ nn_proc.py
@@ -84,91 +84,60 @@ file_test = pickle.load(pf)
 pf.close()
 
 #
-# Split data into train, validation, test sets
-#
-# Two Categories:  US English Speaker vs. Outside US English Speaker
-# 
-###data_len_max=800000 # set maximum input size to NN
-####train_audio = np.zeros([1,data_len_max,1])
-####train_labels = np.zeros([1,1])
-#
-####valid_audio = np.zeros([1,data_len_max,1])
-####valid_labels = np.zeros([1,1])
-
-# Set up training data
-####train_audio[0,:,0] = data_train[:min(data_len_max,len(data_train))]
-####train_labels[0,0] = 1  # US Based speaker.
-# Set up validation data
-####valid_audio[0,:,0] = data_valid[:min(data_len_max,len(data_valid))]
-####valid_labels[0,0] = 0  # Outside US Based speaker.
-#
-# Convert to one-hot encoding
-#
-####train_labels_onehot = np_utils.to_categorical(train_labels, 2)
-####valid_labels_onehot = np_utils.to_categorical(valid_labels, 2)
-#
-# 
-#
-####print('train_audio.shape = ', train_audio.shape)
-####print('valid_audio.shape = ', valid_audio.shape)
-#
-####print('train_labels.shape = ', train_labels.shape)
-####print('valid_labels.shape = ', valid_labels.shape)
-
-####print('train_labels_onehot.shape = ', train_labels_onehot.shape)
-####print('valid_labels_onehot.shape = ', valid_labels_onehot.shape)
-####print('train_labels_onehot = ', train_labels_onehot)
-####print('valid_labels_onehot = ', valid_labels_onehot)
-
-####plt.plot(train_audio[0,:,0])
-####plt.show()
-####plt.plot(valid_audio[0,:,0])
-####plt.show()
-
-#
 # Train NN to classifier vector into US vs. outside US speakers
 #
-####np.random.seed(42)
-####audio_model = Sequential()
+data_len_max = data_train.shape[2] # Common max length
+
+np.random.seed(42)
+
+#
+# Scramble the training data (it is all USA then all OTHER in the file)
+#
+
+perm = np.random.permutation(len(data_train))
+data_train_scramble = data_train[perm]
+label_train_scramble = label_train[perm]
+
 #
 # The architecture should be
 #
 # 1:  num_sound_samples x 512 filters
 #
-# Add the 1D convolution
+# 2:  Add the 1D convolution
 #
-# Max pool down to 256 (number of characters in snippet) x 512 phonemes
+# 3:  Max pool down to 256 (number of characters in snippet) x 512 phonemes
 #
-# Pool sum -> 1x512  (Sum across the entire field)
+# 4:  Pool sum -> 1x512  (Sum across the entire field)
 #
+audio_model = Sequential()
 
-####audio_model.add(Conv1D(128, kernel_size=4096, strides=1, activation='relu', input_shape=(data_len_max,1)))
+audio_model.add(Conv1D(512, kernel_size=4096, strides=1, activation='relu', input_shape=(data_len_max,1)))
 
 # Collapse down to number of characters (should be based on time of snippet)
-####audio_model.add(MaxPooling1D(4096)) #100 time samples per letter
-# Include a two layer MLP for the classifier backend
-####audio_model.add(Dense(512, activation='relu'))
-####audio_model.add(Dropout(0.2))
-####audio_model.add(Dense(512, activation='relu'))
-####audio_model.add(Dropout(0.2))
+audio_model.add(MaxPooling1D(4096)) #100 time samples per letter
+# Include a two layer multi-layer perceptron for the classifier backend
+audio_model.add(Dense(512, activation='relu'))
+audio_model.add(Dropout(0.2))
+audio_model.add(Dense(512, activation='relu'))
+audio_model.add(Dropout(0.2))
 
 # Collapse down to 1,512
-####audio_model.add(GlobalAveragePooling1D())
+audio_model.add(GlobalAveragePooling1D())
 # We want probabilities over the accent classes (2 for now)
-####audio_model.add(Dense(2,activation='softmax'))
+audio_model.add(Dense(2,activation='softmax'))
 
 # Output architecture and compile
-####audio_model.summary()
-####audio_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+audio_model.summary()
+audio_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
 #
 # Compute Cross-Validation Accuracy
 #
-####checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.audio.hdf5', 
-####                               verbose=1, save_best_only=True)
+checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.audio.hdf5', 
+                               verbose=1, save_best_only=True)
 
-####audio_model.fit(train_audio, train_labels_onehot, 
-####          validation_data=(valid_audio, valid_labels_onehot),
-####          epochs=3, batch_size=1, callbacks=[checkpointer], verbose=1)
+audio_model.fit(data_train_scramble, label_train_scramble, 
+          validation_data=(data_valid, label_valid),
+          epochs=1, batch_size=1, callbacks=[checkpointer], verbose=1)
 
 #
 # Compute Test Accuracy
