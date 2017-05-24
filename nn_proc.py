4aa8de7bc87fff5b5a473b4fd08937c0968e89e5
td2014 <anthonyd2004@gmail.com>
2017-05-24 10:43:03 -0700

    This model has valid_accuracy 58%

--- nn_proc.py
+++ nn_proc.py
@@ -10,7 +10,7 @@ Create NN model and evaluate.
 
 from keras.layers import Conv1D
 from keras.layers import Dropout, Flatten, Dense
-from keras.layers import GlobalAveragePooling1D, MaxPooling1D
+from keras.layers import GlobalAveragePooling1D, MaxPooling1D, AveragePooling1D
 from keras.models import Sequential
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
@@ -44,9 +44,10 @@ file_test_pickle=outputDir_pickle_files+'file_test_pickle.out'
 #
 # Load pickle files
 #
-
+subset_len=1000000 # reduce full data fields
 pf=open(data_train_pickle, 'rb')
-data_train = np.load(pf)
+data_train_full = np.load(pf)
+data_train = data_train_full[:,:,:subset_len]
 pf.close()
 
 pf=open(label_train_pickle, 'rb')
@@ -59,7 +60,8 @@ pf.close()
 
 ###
 pf=open(data_valid_pickle, 'rb')
-data_valid = np.load(pf)
+data_valid_full = np.load(pf)
+data_valid = data_valid_full[:,:,:subset_len]
 pf.close()
 
 pf=open(label_valid_pickle, 'rb')
@@ -72,7 +74,8 @@ pf.close()
 
 ###
 pf=open(data_test_pickle, 'rb')
-data_test = np.load(pf)
+data_test_full = np.load(pf)
+data_test = data_test_full[:,:,:subset_len]
 pf.close()
 
 pf=open(label_test_pickle, 'rb')
@@ -113,15 +116,18 @@ data_valid_reshape = np.reshape(data_valid,(-1,data_len_max,1))
 #
 audio_model = Sequential()
 
-audio_model.add(Conv1D(128, kernel_size=4096, strides=10, activation='relu', input_shape=(data_len_max,1)))
+audio_model.add(AveragePooling1D(pool_size=20, strides=None, padding='valid',input_shape=(data_len_max,1)))
+
+#audio_model.add(Conv1D(128, kernel_size=4096, strides=50, activation='relu', input_shape=(data_len_max,1)))
+audio_model.add(Conv1D(256, kernel_size=128, strides=5, activation='relu'))
 
 # Collapse down to number of characters (should be based on time of snippet)
-audio_model.add(MaxPooling1D(4096)) #100 time samples per letter
+audio_model.add(MaxPooling1D(64)) #100 time samples per letter
 # Include a two layer multi-layer perceptron for the classifier backend
 audio_model.add(Dense(512, activation='relu'))
-audio_model.add(Dropout(0.2))
+###audio_model.add(Dropout(0.01))
 audio_model.add(Dense(512, activation='relu'))
-audio_model.add(Dropout(0.2))
+###audio_model.add(Dropout(0.01))
 
 # Collapse down to 1,512
 audio_model.add(GlobalAveragePooling1D())
@@ -139,7 +145,7 @@ checkpointer = ModelCheckpoint(filepath='saved_models/weights.best.audio.hdf5',
 
 audio_model.fit(data_train_scramble, label_train_scramble, 
           validation_data=(data_valid_reshape, label_valid),
-          epochs=1, batch_size=1, callbacks=[checkpointer], verbose=1)
+          epochs=50, batch_size=20, callbacks=[checkpointer], verbose=1)
 
 #
 # Compute Test Accuracy
