b48c43d121f19a95b5a4842182650d5556affc91
td2014 <anthonyd2004@gmail.com>
2017-06-01 16:15:14 -0700

    v17:  testTest accuracy: 55.1020%

--- nn_proc.py
+++ nn_proc.py
@@ -7,22 +7,47 @@ Create NN model and evaluate.
 
 @author: anthonydaniell
 """
+import numpy as np
+import tensorflow as tf
+import random as rn
+np.random.seed(42)
+rn.seed(12345)
+#single thread
+session_conf = tf.ConfigProto(
+      intra_op_parallelism_threads=1,
+      inter_op_parallelism_threads=1)
 
+from keras import backend as K
+tf.set_random_seed(1234)
+sess = tf.Session(graph=tf.get_default_graph(), config=session_conf)
+K.set_session(sess)
+
+#
+from keras.initializers import glorot_uniform
 from keras.layers import Conv1D
 from keras.layers import Dropout, Flatten, Dense
 from keras.layers import GlobalAveragePooling1D, MaxPooling1D, AveragePooling1D
 from keras.layers import GlobalMaxPooling1D
-from keras.models import Sequential
+from keras.models import Sequential, load_model
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
-import numpy as np
+
 import scipy.signal as sig
 import io
-import soundfile as sf
 import matplotlib.pyplot as plt
 import pickle
 import sounddevice as sd
 
+#
+# print out some random numbers to verify we are set up consistently 
+# from run to run
+#
+
+###print('numpy random (start) = ', np.random.uniform())
+###print('python random (start) = ', rn.uniform(0,1))
+###sess = tf.Session()
+###with sess.as_default():
+####print('tensorflow random (start) = ', K.random_uniform_variable((2,3), 0, 1).eval(session=sess))
 
 #
 # Setup some directories and files
@@ -97,8 +122,6 @@ pf.close()
 #
 data_len_max = data_train.shape[2] # Common max length
 
-np.random.seed(42)
-
 #
 # Scramble the training data (it is all USA then all OTHER in the file)
 #
@@ -121,53 +144,53 @@ data_test_reshape = np.reshape(data_test,(-1,data_len_max,1))
 #
 # 4:  Pool sum -> 1x512  (Sum across the entire field)
 #
-model_name='audio_v16_named'
+model_name='audio_v17_named'
 audio_model = Sequential()
 
-###audio_model.add(AveragePooling1D(pool_size=20, strides=None, padding='valid',input_shape=(data_len_max,1)))
-
-audio_model.add(Conv1D(32, kernel_size=512, strides=8, activation='relu', input_shape=(data_len_max,1), name='conv_1'))
-
-###audio_model.add(Conv1D(128, kernel_size=128, strides=5, activation='relu'))
+# Detect speech component in waveforms.
+audio_model.add(Conv1D(64, kernel_size=512, strides=16, activation='relu', 
+                       input_shape=(data_len_max,1), name='conv_1'))
 
 # Collapse down to number of characters (should be based on time of snippet)
-###audio_model.add(MaxPooling1D(16, name='maxpool_1')) #100 time samples per letter
-audio_model.add(Dropout(0.5, name='dropout_0'))
+
 audio_model.add(GlobalMaxPooling1D(name='maxpool_1')) #100 time samples per letter
 # Include a two layer multilayer perceptron for the classifier backend
 audio_model.add(Dense(256, activation='relu', name='dense_1'))
-##audio_model.add(Dropout(0.7, name='dropout_1'))
-##audio_model.add(Dense(256, activation='relu', name='dense_2'))
-##audio_model.add(Dropout(0.1, name='dropout_2'))
+audio_model.add(Dropout(0.5, name='dropout_1'))
 
-# Collapse down to 1,512
-###audio_model.add(GlobalAveragePooling1D(name='gap_1'))
 # We want probabilities over the accent classes (2 for now)
 audio_model.add(Dense(2,activation='softmax', name='dense_3'))
 
 # Output architecture and compile
 audio_model.summary()
-audio_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+###audio_model.compile(loss='categorical_crossentropy', optimizer='rmsprop', metrics=['accuracy'])
+audio_model.compile(loss='categorical_crossentropy', optimizer='adagrad', metrics=['accuracy'])
 #
 # Compute Cross-Validation Accuracy
 #
 modelweights_filepath=savedmodel_path+'weights.best_'+model_name+'.hdf5'
-checkpointer = ModelCheckpoint(filepath=modelweights_filepath, 
-                               verbose=1, save_best_only=False)
+###checkpointer = ModelCheckpoint(filepath=modelweights_filepath, 
+###                              verbose=1, save_best_only=False)
+
+###audio_model.fit(data_train_scramble, label_train_scramble, 
+###          validation_data=(data_valid_reshape, label_valid),
+###          epochs=1, batch_size=20, callbacks=[checkpointer], shuffle=False, verbose=0)
 
 audio_model.fit(data_train_scramble, label_train_scramble, 
           validation_data=(data_valid_reshape, label_valid),
-          epochs=10, batch_size=20, callbacks=[checkpointer], verbose=1)
+          epochs=20, batch_size=20, shuffle=True, verbose=1)
+
 
 #
 # Save full_model
 #
-
 audio_model.save(savedmodel_path+'config_'+model_name+'.hdf5')
+# make sure we got a good saved model.
+del audio_model 
 #
 # Compute Test Accuracy
 #
-audio_model.load_weights(modelweights_filepath)
+audio_model = load_model(savedmodel_path+'config_'+model_name+'.hdf5')
 
 # get index of predicted accent for each image in test set
 audio_predictions = [np.argmax(audio_model.predict(np.expand_dims(feature, axis=0))) for feature in data_test_reshape]
@@ -175,6 +198,19 @@ audio_predictions = [np.argmax(audio_model.predict(np.expand_dims(feature, axis=
 # report test accuracy
 test_accuracy = 100*np.sum(np.array(audio_predictions)==np.argmax(label_test, axis=1))/len(audio_predictions)
 print('Test accuracy: %.4f%%' % test_accuracy)
+
+###del audio_model
+
+#
+# Check random state at end of processing
+#
+
+###print('numpy random (end) = ', np.random.uniform())
+###print('python random (end) = ', rn.uniform(0,1))
+##sess = tf.Session()
+##with sess.as_default():
+##    print('tensorflow random (end) = ', tf.random_uniform(shape=(1,)).eval())
+
 
 #
 # End of script
---
+++ setupEnviron.sh
@@ -0,0 +1,4 @@
+#!/bin/ash
+# To control random hash behavior in python
+export PYTHONHASHSEED=0
+spyder
